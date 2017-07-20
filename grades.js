ea49598cc316734d4e90b9e85335edce5c23dcf5
DanielBabcock <babcock.daniel.b@gmail.com>
2017-07-20 16:35:24 -0500

    parts are working

---
+++ grades.js
@@ -0,0 +1,72 @@
+
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var a = {
+    from: 91,
+    to: 100
+};
+// var
+var b = {
+    from: 81,
+    to: 91
+};
+// var B = 81-90;
+var c = {
+    from: 71,
+    to: 81
+};
+// var C = 71-80;
+var d = {
+    from: 61,
+    to: 71
+};
+// var D = 61-70;
+var f = {
+    from: 0,
+    to: 60
+};
+var A = 0;
+var B = 0;
+var C = 0;
+var D = 0;
+var F = 0;
+
+// Use console.log() to output the following criteria:
+
+// How many of each grade?
+// What is the lowest grade?
+// What is the highest grade?
+
+for (var i = 0; i < scores.Length; i++) {
+    // if() {
+    //     A ++;
+    //         if else() {
+    //             B ++;
+    //                 if else() {
+    //                      C ++;
+    //                         if else() {
+    //                             D ++;
+    //                                if else() {
+    //                                     F ++;
+    //                                }
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+                };
+
+
+console.log(A,  B,  C,  D, F);
+scores.sort()
+console.log(scores[0]);
+
+console.log(scores[scores.length - 1]);
+
+// vreate var for each grade abcdf of var _ = 0 
+// create for loop to iterate through Array
+//     if loop
+//         statement to add to vars above
+//             if else
+//              statement to add to vars above
+//                 if
+//                     statement to add to vars above
